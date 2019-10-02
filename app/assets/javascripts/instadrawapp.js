89ee6593ef922ae077081df8d3d114d62a3dee4d
NakatsuboYusuke <>
2019-10-02 15:20:51 +0900

    スタイルシートを適用し、崩れた部分のマークアップを書き換えました

--- app/assets/javascripts/instadrawapp.js
+++ app/assets/javascripts/instadrawapp.js
@@ -1,5 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  // hamburger memu
+  const hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active')
+  });
+
+  const wrapper = document.getElementById('wrapper');
+  const navBtn = document.getElementById('nav-btn');
+
+  navBtn.addEventListener('click', navToggle);
+  function navToggle() {
+    if (wrapper.classList.contains('nav-open')) {
+      navCloseFunc();
+    } else {
+      navOpenFunc();
+    }
+  }
+
+  const navOpenFunc = () => {
+    wrapper.classList.add('nav-open');
+  }
+  const navCloseFunc = () => {
+    wrapper.classList.remove('nav-open');
+  }
+
+  // modal
   const js_modal_open = document.getElementsByClassName('js-modal-open');
 
   for (let i = 0; i < js_modal_open.length; i++) {
